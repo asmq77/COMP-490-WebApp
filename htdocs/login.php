f616aaea1b28b3ded07a97db3b368546cadeb8b2
JDaskalov <juliandaskalov@gmail.com>
2016-09-23 14:05:21 -0700

    Add files via upload
    Login page

---
+++ htdocs/login.php
@@ -0,0 +1,87 @@
+
+<!DOCTYPE HTML>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>Login</title>
+  <meta name="description" content="Development Guidelines for Developers Implementing Metaphors UI Components.">
+
+  <!-- FONT LIBS -->
+  <script src="//use.typekit.net/gfb2mjm.js"></script>
+  <script>try{Typekit.load();}catch(e){}</script>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic">
+  <link rel="stylesheet" href="./bower_components/metaphor/dist/css/metaphor.css">  
+</head>
+<body>
+
+  <div class="header">
+    <nav class="primary-nav">
+      <div class="container">
+        <div class="primary-nav__mobile">
+          <div class="primary-nav__btn">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+          </div>
+          <div class="primary-nav__brand"><span class="sr-only"></span></div>
+          <a class="sr-only" href="#main">Skip to main content</a>
+        </div>
+        <ul class="primary-nav__links">
+          <li><a href="index.html" class="primary-nav__link">Home</a></li>
+          <li><a href="about.html" class="primary-nav__link">About</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+
+  <div class="section" style="background-color: #252525;">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <h1 class="type--white type--thin type--marginless">Login</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+
+   <div align = center>
+    <h2>Please enter your user information</h2>
+    <form action="temp.php" method="post">
+      E-mail: <input type="text" name="E-mail" style="width: 20em;"><br>
+      Password: <br><input type="password" name="Password" style="width: 20em;"><br>
+      <input type="Submit">
+      <h3></h3>
+    </form>
+  </div>
+
+<div class="metalab-footer">
+  <div class="metalab-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6">
+          
+        </div>
+        <div class="col-sm-6">
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- SCRIPTS -->
+<script src="./bower_components/metaphor/dist/js/metaphor.js"></script>
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-77781058-1', 'auto');
+ga('send', 'pageview');
+</script>
+</body>
+</html>
