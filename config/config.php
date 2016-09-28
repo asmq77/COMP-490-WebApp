8187c8b18d0c856672dc702a0685822228109295
Asad <aabdullah7757@gmail.com>
2016-09-28 09:12:21 -0700

    added createProj.php, working on it

--- config/config.php
+++ config/config.php
@@ -1,29 +1,29 @@
 <?php
 //Heroku format
-// $herokudb = array(
-// 	'mysql'  => array(
-// 	    'driver'    => 'mysql',
-// 	    'host'      => 'us-cdbr-iron-east-04.cleardb.net',
-// 	    'database'  => 'heroku_8e6edb63bd8e722',
-// 	    'username'  => 'b2b6d4f62d70b0',
-// 	    'password'  => 'd2a42c67',
-// 	    'charset'   => 'utf8',
-// 	    'collation' => 'utf8_unicode_ci',
-// 	    'prefix'    => '',
-// 	    'strict'    => false,
-// ));
-
-//Localhost format
 $herokudb = array(
 	'mysql'  => array(
 	    'driver'    => 'mysql',
-	    'host'      => 'localhost',
-	    'database'  => 'proposer',
-	    'username'  => 'root',
-	    'password'  => 'root',
+	    'host'      => 'us-cdbr-iron-east-04.cleardb.net',
+	    'database'  => 'heroku_8e6edb63bd8e722',
+	    'username'  => 'b2b6d4f62d70b0',
+	    'password'  => 'd2a42c67',
 	    'charset'   => 'utf8',
 	    'collation' => 'utf8_unicode_ci',
 	    'prefix'    => '',
 	    'strict'    => false,
 ));
+
+//Localhost format
+// $herokudb = array(
+// 	'mysql'  => array(
+// 	    'driver'    => 'mysql',
+// 	    'host'      => 'localhost',
+// 	    'database'  => 'proposer',
+// 	    'username'  => 'root',
+// 	    'password'  => 'root',
+// 	    'charset'   => 'utf8',
+// 	    'collation' => 'utf8_unicode_ci',
+// 	    'prefix'    => '',
+// 	    'strict'    => false,
+// ));
 ?>
