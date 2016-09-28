b3aaf64dbdec39555fd3d11250dbf362f53ebd4a
inkyu Park <ip278116@gmail.com>
2016-09-27 20:15:57 -0700

    edit

--- dbh.php
+++ dbh.php
@@ -6,4 +6,10 @@ if (!$conn) {
 	die("Connection failed: ".mysqli_connect_error()); 
 }
 
+$conn = mysqli_connect("evening-tor-76502.herokuapp.com", "b2b6d4f62d70b0", "d2a42c67", "logintest");
+
+if (!$conn) {
+	die("Connection failed: ".mysqli_connect_error()); 
+}
+
 ?>
