934b884ab8095e88e5a24daea5d8e83dbba12ebf
inkyu Park <ip278116@gmail.com>
2016-09-30 10:55:38 -0700

    fix errors

--- index.php
+++ index.php
@@ -3,20 +3,21 @@ session_start();
 
 include './config/common.php';
 
-$uid = $_POST['uid'];
-$pwd = $_POST['pwd'];
+if (isset($_POST['login'])){ 
+  $uid = $_POST['uid'];
+  $pwd = $_POST['pwd'];
 
-$dbh = getPDO();
-$stmt = $dbh->query("SELECT * FROM users");
-  
-while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-  if($row['uid'] == $uid && $row['pwd'] == $pwd) {
-    $_SESSION['uid'] = $uid;
+  $dbh = getPDO();
+  $stmt = $dbh->query("SELECT * FROM users");
+    
+  while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    if($row['uid'] == $uid && $row['pwd'] == $pwd) {
+      $_SESSION['uid'] = $uid;
 
-    header('Location: login.php');    
-  } 
+      header('Location: login.php');    
+    } 
+  }
 }
-
 
 ?>
 
